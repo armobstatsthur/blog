67433bf10f04a5cefd309eba42cb537d286a5852
Arthur Moura Carvalho <armoucar@gmail.com>
2013-12-27 04:30:12 +0000

    new blogpost. One month and a half has passed ;).

--- javascripts/application.js
+++ javascripts/application.js
@@ -9,20 +9,42 @@
     responsiveUrl.href = "http://armoucar.github.io/responsive-play/?to=1600&url=http%3A%2F%2Farthurcarvalho.com%2Fdont-panic%2F";
   }
 
-  if (win.document.querySelector("#maps-ireland")) {
+  (function () {
+    if (win.document.querySelector("#maps-ireland")) {
+      win.inicializaMapa =  function() {
+        var mapOptions = {
+          zoom: 13,
+          center: new google.maps.LatLng(53.34771, -6.2592),
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(document.getElementById("maps-ireland"), mapOptions);
+      };
 
-    win.inicializaMapa =  function() {
-      var mapOptions = {
-        zoom: 13,
-        center: new google.maps.LatLng(53.34771, -6.2592),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      }
-      var map = new google.maps.Map(document.getElementById("maps-ireland"), mapOptions);
-    };
+      var script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyBpmmPNvZHnfJZZB8JUlCdbbV9tO-s-7pI&sensor=false&callback=inicializaMapa";
+      win.document.body.appendChild(script);
+    }
+  }());
 
-    var script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyBpmmPNvZHnfJZZB8JUlCdbbV9tO-s-7pI&sensor=false&callback=inicializaMapa";
-    win.document.body.appendChild(script);
-  }
+  (function () {
+    if (win.document.querySelector("#maps-clondalkin")) {
+      win.inicializaMapa =  function() {
+        var mapOptions = {
+          zoom: 13,
+          center: new google.maps.LatLng(53.32180, -6.3940),
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(document.getElementById("maps-clondalkin"), mapOptions);
+      };
+
+      var script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyBpmmPNvZHnfJZZB8JUlCdbbV9tO-s-7pI&sensor=false&callback=inicializaMapa";
+      win.document.body.appendChild(script);
+    }
+  }());
+
+
+
 })(window, CLIType);
